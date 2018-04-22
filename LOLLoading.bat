2c15cab517627b998f1aa31bfc15a26ab4d1d59d
bjc5233 <bjc5233@gmail.com>
2018-04-23 04:25:05 +0800

    add common help funs

--- LOLLoading.bat
+++ LOLLoading.bat
@@ -1,17 +1,23 @@
-@echo off& call load.bat _getDeskWallpaperPath& call loadE.bat imagemagick-convert zipMini elevate& setlocal enabledelayedexpansion
-::说明
-::  更改LOL载入界面背景
-::参数
-::  [imgPath]
-::      imgPath - 图片地址；无则使用当前桌面壁纸
-::external
-::  date       2018-02-06 23:05:10
-::  face       ●︿●
-::  weather    shanghai 晴 4℃/-2℃ 北风
+@echo off& call load.bat _getDeskWallpaperPath& call loadF.bat _help& call loadE.bat imagemagick-convert zipMini elevate& setlocal enabledelayedexpansion
+:::说明
+:::  更改LOL载入界面背景
+:::参数
+:::  [imgPath]
+:::      imgPath - 图片地址；无则使用当前桌面壁纸
+:::  [help\-help\-h]
+:::      help - 打印注释信息
+:::external
+:::  date       2018-02-06 23:05:10
+:::  face       ●︿●
+:::  weather    shanghai 晴 4℃/-2℃ 北风
+
 set lolPath=C:\Program Files\腾讯游戏\英雄联盟
 if "%~1" EQU "" (
 	(%_call% ("wallpaperPath") %_getDeskWallpaperPath%)
 ) else (
+	if "%~1" EQU "help" (call %_help% "%~f0"& goto :EOF)
+	if "%~1" EQU "-help" (call %_help% "%~f0"& goto :EOF)
+	if "%~1" EQU "-h" (call %_help% "%~f0"& goto :EOF)
 	set wallpaperPath=%~1
 )
 
